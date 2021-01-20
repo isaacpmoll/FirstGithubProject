4f23ca367a33280b5c0da61cf9bbc31d9b83722b
isaacpmoll <73903177+isaacpmoll@users.noreply.github.com>
2021-01-20 12:23:39 -0500

    Update DoThis.py

--- DoThis.py
+++ DoThis.py
@@ -2,3 +2,10 @@
 # ask the user for two numbers they want added,
 # add them together and return the result. 
 # then commit and push it back up to github.
+
+print("Isaac Moll")
+
+num1 = int(input("Gimme number one: "))
+num2 = int(input("Gimme number two: "))
+
+print(num1+num2)
